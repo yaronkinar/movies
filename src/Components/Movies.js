b5f54e5926548216b4dc6279513e6db6a7aaa834
yaron <yaronkinar@gmail.com>
2022-05-04 12:02:49 +0300

    second commit

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,55 +1,67 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 import Movie from "./Movie";
+
 // https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json
 function Movies() {
 
-    const [movies,setMovies] = useState([])
+    const [movies, setMovies] = useState([])
+    const [sortAscending, setSortAscending] = useState(false)
     useEffect(function () {
         let url = " https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json";
-        axios.get(url).then(r  =>{
+        axios.get(url).then(r => {
             let data = r.data
             setMovies(data)
 
         })
-    },[])
-    function sortMovies(){
+    }, [])
+    useEffect(function () {
+        if (sortAscending) {
+            sortMovies()
+        } else {
+            sortBackwordMovies()
+        }
+    }, [sortAscending])
 
-        const sortedList = [...movies].sort((a, b) =>{
+    function sortMovies() {
+        const sortedList = [...movies].sort((a, b) => {
             return a.title.localeCompare(b.title)
-        } );
+        });
 
 
         setMovies(sortedList)
-      //  setToggleButton(true)
 
     }
-    function sortBackwordMovies(){
+
+    function handleToggle() {
+        setSortAscending(!sortAscending)
+    }
+
+    function sortBackwordMovies() {
         const sortedList = [...movies].sort((a, b) => {
             return b.title.localeCompare(a.title);
         })
 
         setMovies(sortedList)
-      //  setToggleButton(false)
 
     }
+
     return (
         <div className="container">
             <h1>Movies</h1>
             <div className="buttons">
-                <button className="sort dec" onClick={sortMovies}>
-                    Sort Descending
+                <button className="sort dec" onClick={handleToggle}>
+                    Sort By Title
                 </button>
-                <button className="sort dec" onClick={sortBackwordMovies}>
+                {/*<button className="sort dec" onClick={sortBackwordMovies}>
                     Sort Ascending
-                </button>
+                </button>*/}
             </div>
 
 
-
             <div className="movieList">
-                {movies.slice(0,9)?.map(function (movie,id) {
-                    return  (<Movie key={movie.title}  id={id}{...movie}/>)
+                {movies.slice(0, 9)?.map(function (movie, id) {
+                    return (<Movie key={movie.title} id={id}{...movie}/>)
                 })}
             </div>
 
