365c0be6b7ae8017a79c4d06838ea811d9428943
yaron <yaronkinar@gmail.com>
2022-05-07 10:36:05 +0300

    callback  fix

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import Movie from "./Movie";
 
 // https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json
 function Movies() {
 
     const [movies, setMovies] = useState([])
- //   const [sortAscending, setSortAscending] = useState(false)
+    const [sortAscending, setSortAscending] = useState(false)
     useEffect(function () {
         let url = " https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json";
         axios.get(url).then(r => {
@@ -15,29 +15,40 @@ function Movies() {
 
         })
     }, [])
-   /* useEffect(function () {
-        function sortMovies() {
-            const sortedList = [...movies].sort((a, b) => {
-                return a.title.localeCompare(b.title)
-            });
-            setMovies(sortedList)
-        }
-        function sortBackwardMovies() {
-            const sortedList = [...movies].sort((a, b) => {
-                return b.title.localeCompare(a.title);
-            })
+    const sortMoviesCb = useCallback(() => sortMovies(movies), [movies]);
+    const sortBackwardMoviesCB = useCallback(() => sortBackwardMovies(movies), [movies]);
 
-            setMovies(sortedList)
+    /* useEffect(function () {
+         function sortMovies() {
+             const sortedList = [...movies].sort((a, b) => {
+                 return a.title.localeCompare(b.title)
+             });
+             setMovies(sortedList)
+         }
+         function sortBackwardMovies() {
+             const sortedList = [...movies].sort((a, b) => {
+                 return b.title.localeCompare(a.title);
+             })
 
-        }
+             setMovies(sortedList)
 
-        if (sortAscending) {
-            sortMovies()
-        } else {
-            sortBackwardMovies()
-        }
-    }, [sortAscending])*/
-    function sortMovies() {
+         }
+
+         if (sortAscending) {
+             sortMovies()
+         } else {
+             sortBackwardMovies()
+         }
+     }, [sortAscending])*/
+    function sortBackwardMovies(movies) {
+        const sortedList = [...movies].sort((a, b) => {
+            return b.title.localeCompare(a.title);
+        })
+
+        setMovies(sortedList)
+
+    }
+    function sortMovies(movies) {
         const sortedList = [...movies].sort((a, b) => {
             return a.title.localeCompare(b.title)
         });
@@ -45,9 +56,16 @@ function Movies() {
     }
 
 
-    function handleToggle() {
-      //  setSortAscending(!sortAscending)
-        sortMovies()
+    function handleToggle(sort) {
+        setSortAscending(!sort)
+        console.log(sortAscending)
+        if(sortAscending){
+            sortMoviesCb()
+
+        }else {
+            sortBackwardMoviesCB()
+
+        }
     }
 
 
@@ -56,12 +74,10 @@ function Movies() {
         <div className="container">
             <h1>Movies</h1>
             <div className="buttons">
-                <button className="sort dec" onClick={handleToggle}>
+                <button className="sort dec" onClick={()=>{handleToggle(sortAscending)}}>
                     Sort By Title
                 </button>
-                {/*<button className="sort dec" onClick={sortBackwordMovies}>
-                    Sort Ascending
-                </button>*/}
+
             </div>
 
 
